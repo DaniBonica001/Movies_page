08f196779b282485fd2bc4fdccb3b7562583a1f5
DaniBonica001 <danielabc_0106@hotmail.com>
2023-01-20 18:07:07 -0500

    module component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,11 @@
-import "../components/MovieCard.css";
+import styles from"../components/MovieCard.module.css";
 import { Link } from "react-router-dom";
 
 export function MovieCard({ movie }) {
   const imageUrl = `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
   return (
-    <li className="movieCard">
-      <Link to={"/movies/"+movie.id}>
+    <li className={styles.movieCard}>
+      <Link to={"/movies/" + movie.id}>
         <img
           className="movieImage"
           width={230}
@@ -13,8 +13,8 @@ export function MovieCard({ movie }) {
           src={imageUrl}
           alt={movie.title}
         />
-        <div> {movie.title} </div>
       </Link>
+      <div className={styles.title}> {movie.title} </div>
     </li>
   );
 }
